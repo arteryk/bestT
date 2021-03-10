a7e2d2d8a06460c8ab092be64d4f380f4ffbef1f
Nikolay Kalchev <nikolay.kalchev@partymus.com>
2021-03-10 07:53:47 +0200

    beforeWeBreak
    starting work on the actual to do T1

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/toDos', (req, res) => {
-  res.send([1, 2, 3]);
+  res.send([1, 2, 3, 4]);
 });
 
-app.listen(3000, () => console.log('Listening on Port 3000!'));
+// /api/toDos/1
+app.get('/api/toDos/:toDoID', (req, res) => {
+  res.send(req.params.toDoID);
+});
+
+// /api/posts/1
+app.get('/api/posts/:year/:month', (req, res) => {
+  res.send(req.params);
+});
+
+// PORT
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on Port ${port}!...`));
 
